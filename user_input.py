a5cbcb5d472af40c658c2d5107dd68ca33bbd539
susan <susangithaiga26@gmail.com>
2024-11-04 07:52:40 +0300

    complete

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("State your location: ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}")
